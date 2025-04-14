f235ae812082abd95259b2730f5d59198b891f99
Felix.rosado <felix.rosado@grupocsi.com>
2025-04-14 10:14:11 -0600

    Crear componente Lista De Tareas. Borra componente autogenerados. Crear rutas

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="sidebar">
+        <h2>Barra Lateral</h2>
+        <ul>
+        <li><Router-link to="/contador">Contador</Router-link></li>
+        <li><Router-link to="/lista-de-tareas">Lista De Tareas</Router-link></li>
+        </ul>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style>
+.sidebar {
+    width: 200px;
+    background-color: #f0f0f0;
+    padding: 20px;
+}
+</style>
