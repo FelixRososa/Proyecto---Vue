270c88a07468d31dc17af8e53e4bb5c4a29033e0
Felix.rosado <felix.rosado@grupocsi.com>
2025-04-15 10:57:39 -0600

    Modificacion temperatura

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="header">
+        <Weather></Weather>
+    </div>
+</template>
+<script setup>
+import Weather from './Weather.vue';
+
+</script>
+<style scoped>
+.header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 20px;
+    top: 0;
+    right: 0;
+    left: 0;
+    background-color: #333;
+    color: white;
+}
+h3{
+    margin: 0;
+}
+</style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="sidebar">
         <h2>Bienvenido</h2>
-        <h3>Nombre: {{ nombre }}</h3>
-        <h3>Email:{{ email }}</h3>
+        <h3>Nombre: {{ registrarStore.nombre }}</h3>
+        <h3>Email:{{ registrarStore.email }}</h3>
         <ul>
         <li><Router-link to="/contador">Contador</Router-link></li>
         <li><Router-link to="/lista-de-tareas">Lista De Tareas</Router-link></li>
@@ -12,16 +12,9 @@
 </template>
 <script setup>
 import { useRegistrarStore } from '@/Modules/registro/stores/registrarStore';
-import{ref, watch} from 'vue';
+
 const registrarStore = useRegistrarStore();
-const nombre = ref(registrarStore.nombre.value);
-const email = ref(registrarStore.nombre.value);
-watch(() => registrarStore.nombre, (newValue) => {
-    nombre.value = newValue;
-});
-watch(() => registrarStore.email, (newValue) =>{
-    email.value = newValue;
-})
+
 
 </script>
 <style>
